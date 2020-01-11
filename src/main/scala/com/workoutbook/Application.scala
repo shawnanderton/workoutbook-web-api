9058196e1bca29d9d7d34b514a38a95e2e0da7ab
Shawn Anderton <shawnanderton1@gmail.com>
2020-01-11 07:55:31 -0700

    remove unneeded code

--- src/main/scala/com/workoutbook/Application.scala
+++ src/main/scala/com/workoutbook/Application.scala
@@ -1,16 +1,12 @@
 package com.workoutbook
 
 import org.springframework.boot.SpringApplication
-import org.springframework.boot.autoconfigure.{EnableAutoConfiguration, SpringBootApplication}
-import org.springframework.context.annotation.ComponentScan
+import org.springframework.boot.autoconfigure.SpringBootApplication
 
 @SpringBootApplication
-@EnableAutoConfiguration
-@ComponentScan
 class Application
 
 
 object Application extends App {
-
     SpringApplication.run(classOf[Application])
 }
